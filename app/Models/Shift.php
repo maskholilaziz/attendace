7ba2b9bd6cdae6b86ba0a57c250870155642133c
Muhammad Kholil Aziz Ashari <emkahaaa@gmail.com>
2025-10-02 15:37:47 +0700

    CRUD Shift & Office

--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Shift extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'start_time',
+        'end_time',
+    ];
 }
