2ab5165359b1acfb78678e0e6c72d5f2a7b9ba7d
Muhammad Kholil Aziz Ashari <emkahaaa@gmail.com>
2025-10-03 17:44:06 +0700

    Add LiveWire for Attendance

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Livewire\Attendance;
 use Illuminate\Support\Facades\Route;
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('attandance', Attendance::class)->name('attandance');
+});
 
 Route::get('/', function () {
     return view('welcome');
