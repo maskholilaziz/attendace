b6bc992c41bc276e6b4f24a3c3abce842e0c09fd
Muhammad Kholil Aziz Ashari <emkahaaa@gmail.com>
2025-10-06 10:56:22 +0700

    Add leaflet

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <style>
+        #map {
+            height: 400px;
+        }
+    </style>
     <title>Attendance</title>
 </head>
 
--- resources/views/livewire/attendance.blade.php
+++ resources/views/livewire/attendance.blade.php
@@ -1,3 +1,25 @@
 <div>
-    To attain knowledge, add things every day; To attain wisdom, subtract things every day.
+    <div class="container mx-auto">
+        <div class="bg-white p-6 rounded-lg mt-3 shadow-lg">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+                <div>
+                    <h2 class="text-2xl font-bold mb-2">Informasi Pegawai</h2>
+                    <div class="bg-gray-100 p-4 rounded-lg">
+                        <p><strong>Nama Pegawai: </strong>{{ Auth::user()->name }}</p>
+                        <p><strong>Kantor: </strong>Kantor Utama</p>
+                        <p><strong>Shift: </strong>Pagi</p>
+                    </div>
+                </div>
+                <div>
+                    <h2 class="text-2xl font-bold mb-2">Attendance</h2>
+                    <div id="map" class="mb-4 rounded-lg border border-gray-300"></div>
+                    <button type="button" class="px-4 py-2 bg-blue-500 text-white rounded">Tag Location</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        const map = L.map('map').setView([51.505, -0.09], 13);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    </script>
 </div>
