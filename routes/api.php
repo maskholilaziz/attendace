b21b7a58e1de4c4d1299cd34b6beed6b8e1d3477
Muhammad Kholil Aziz Ashari <emkahaaa@gmail.com>
2025-10-13 17:41:28 +0700

    Get Attendance Today & This Month

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\API\AttendanceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 
 Route::prefix('v1')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
+
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+        Route::get('get-attendance-today', [AttendanceController::class, 'getAttendanceToday']);
+    });
 });
 
 
